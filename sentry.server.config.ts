2da90fa4e3f721d2d3663ece0d219f27ac6b225a
Pascal George <tamunoemi@gmail.com>
2025-02-24 15:38:21 -0500

    feat: integrate Sentry for error tracking and monitoring - Add SDK, enable component annotations, session replay, source maps, and monitoring

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://88c77db9a841e80677cfeb5f2cc543b2@o4508558239924224.ingest.us.sentry.io/4508875831640064",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
